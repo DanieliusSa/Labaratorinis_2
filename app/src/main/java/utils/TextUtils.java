9ff231479a73eb456729080497d6434fcb0eed15
DanieliusSa <danielsavickas@gmail.com>
2022-06-10 18:15:23 +0300

    Labaratorinis 2

---
+++ app/src/main/java/utils/TextUtils.java
@@ -0,0 +1,8 @@
+package com.example.labaratorinis2.utils;
+public class TextUtils {
+    public static int getCharsCount(String input){
+        return input.length();
+    }
+    public static int getWordsCount(String input){ return input.length();    }
+
+}
